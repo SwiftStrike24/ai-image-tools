cd9a81ecc0c661abebc48d20324b144910814bd3
SwiftStrike24 <anwordavid@gmail.com>
2024-09-19 18:39:35 -0600

    added new before and after image.

--- src/components/client-side/landing-pageClient.tsx
+++ src/components/client-side/landing-pageClient.tsx
@@ -264,6 +264,7 @@ export default function LandingPage() {
       { before: '/images/landing-page/before-after-images/before-image2.webp', after: '/images/landing-page/before-after-images/after-image2.jpg' },
       { before: '/images/landing-page/before-after-images/before-image.jpg', after: '/images/landing-page/before-after-images/after-image.jpg' },
       { before: '/images/landing-page/before-after-images/before-image3.webp', after: '/images/landing-page/before-after-images/after-image3.jpg' },
+      { before: '/images/landing-page/before-after-images/before-image4.webp', after: '/images/landing-page/before-after-images/after-image4.jpg' },
     ])
   }, [])
 
